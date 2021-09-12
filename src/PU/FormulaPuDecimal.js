25e6e648fe9f3977387726a9c3d8ff16ae331327
Matheus Vicente <mga@vortx.com.br>
2021-09-12 22:22:04 +0000

    ajuste no calculo + correção de testes

--- src/PU/FormulaPuDecimal.js
+++ src/PU/FormulaPuDecimal.js
@@ -1,43 +1,50 @@
-const Decimal = require('decimal.js');
-const um = new Decimal(1);
+const Decimal = require('decimal.js')
+const um = new Decimal(1)
 
-function calculoPuPos(mediaCdi, porcentagem, dp, fatorDiAcumulado, vne) {
+function calculoPuPos(mediaCdi, porcentagem, dp, fatorDiAcumuladoAnterior, vne) {
 
-    const mediaCdiB = new Decimal(mediaCdi);
-    const porcentagemB = new Decimal(porcentagem);
-    const dpB = new Decimal(dp);
-    const fatorDiAcumuladoB = new Decimal(fatorDiAcumulado);
-    const vneB = new Decimal(vne);
+    const mediaCDIDecimal = new Decimal(mediaCdi)
+    const porcentagemDecimal = new Decimal(porcentagem)
+    const DpDecimal = new Decimal(dp)
+    const fatorDiAcumuladoDecimal = new Decimal(fatorDiAcumuladoAnterior)
+    const vneDecimal = new Decimal(vne)
 
-    const fatordi = calculoFatorDi(mediaCdiB);
-    const fatorspread = calculoFatorSpread(porcentagemB, dpB);
-    const fatordiacumulado = fatorDiAcumuladoB.times(fatordi);
-    const fatorjuros = (fatordiacumulado.times(fatorspread));
+    const di = calculoFatorDi(mediaCDIDecimal)
+    const spread = calculoFatorSpread(porcentagemDecimal, DpDecimal)
+    const diAcumulado = fatorDiAcumuladoDecimal.times(di)
+    const fatorJuros = diAcumulado.times(spread)
 
-    const pu = (vneB).times(fatorjuros);
-    return Number(pu)
+    const pu = vneDecimal.times(fatorJuros)
+
+    return {
+        spread: spread.toDecimalPlaces(14, Decimal.ROUND_DOWN).toNumber(),
+        di: di.toDecimalPlaces(14, Decimal.ROUND_UP).toNumber(),
+        diAcumulado: diAcumulado.toDecimalPlaces(14, Decimal.ROUND_UP).toNumber(),
+        fatorJuros: fatorJuros.toDecimalPlaces(14, Decimal.ROUND_UP).toNumber(),
+        pu: pu.toDecimalPlaces(10, Decimal.ROUND_UP).toNumber(),
+    }
 }
 
 function calculoFatorDi(mediaCdiB) {
 
     if (!(mediaCdiB instanceof Decimal)) mediaCdiB = new Decimal(mediaCdiB)
 
-    const doisCincoDois = new Decimal(252);
-    const cem = new Decimal(100);
-    const div1por252 = (um.dividedBy(doisCincoDois));
-    return ((mediaCdiB.dividedBy(cem)).plus(um)).pow(div1por252);
+    const doisCincoDois = new Decimal(252)
+    const cem = new Decimal(100)
+    const div1por252 = (um.dividedBy(doisCincoDois))
+    return ((mediaCdiB.dividedBy(cem)).plus(um)).pow(div1por252)
 }
 
-function calculoFatorSpread(porcentagemB, dpB) {
-    const doisCincoDois = new Decimal(252);
-    const divisaoDp = new Decimal(dpB.dividedBy(doisCincoDois));
-    return new Decimal(um.plus(porcentagemB)).pow(divisaoDp);
+function calculoFatorSpread(porcentagemDecimal, dpB) {
+    const doisCincoDois = new Decimal(252)
+    const divisaoDp = new Decimal(dpB.dividedBy(doisCincoDois))
+    return new Decimal(um.plus(porcentagemDecimal)).pow(divisaoDp)
 }
-function calculoPuPre(vneB, porcentagemB, dpB) {
+function calculoPuPre(vneDecimal, porcentagemDecimal, dpB) {
     dpB = new Decimal(dpB)
-    const doisCincoDois = new Decimal(252);
-    let divisaoFinal = dpB.dividedBy(doisCincoDois);
-    const pu = new Decimal(vneB).times((new Decimal(um.plus(porcentagemB)).pow(divisaoFinal)));
+    const doisCincoDois = new Decimal(252)
+    let divisaoFinal = dpB.dividedBy(doisCincoDois)
+    const pu = new Decimal(vneDecimal).times((new Decimal(um.plus(porcentagemDecimal)).pow(divisaoFinal)))
     return Number(pu)
 }
 
--- src/PU/FormulaPuDecimal.test.js
+++ src/PU/FormulaPuDecimal.test.js
@@ -1,62 +1,29 @@
 const { expect } = require('chai')
 
 const { calculoPuPos, calculoFatorDi, calculoPuPre } = require('./FormulaPuDecimal')
-const { mediasCdi, puEsperado } = require('./MassaDeTeste/MassaDeTestePosFixado')
-const { puEsperadoPre } = require('./MassaDeTeste/MassaDeTestePreFixado')
-const Decimal = require('decimal.js');
 
 describe('Deve realizar o calculo de pu incorretamente de um ativo pós-fixado utilizando a lib Decimal', () => {
 
     it('Deve calcular o PU', () => {
-        // Massa
         let mediaCdi = 4.15;
         let dp = 36;
         let porcentagem = 0.06;
-        let fatorDiAcumuladoAnterior = 1.00531744
-        let vne = 10006;
-        // Comportamento
-        const puFinal = calculoPuPos(
-            mediaCdi,
-            porcentagem,
-            dp,
-            fatorDiAcumuladoAnterior,
-            vne)
+        let fatorDiAcumuladoAnterior = 1.00531743243777
+        let vne = 10006.00
 
+        const puFinal = calculoPuPos(mediaCdi, porcentagem, dp, fatorDiAcumuladoAnterior, vne)
 
-        // Resultado esperado
-        expect(puFinal).to.be.equal(10144.92670604)
-    })
+        const spreadEsperado = 1.00835887163168
+        const diEsperado = 1.00016137008907
+        const diAcumuladoEsperado = 1.00547966060139
+        const fatorJurosEsperado = 1.01388433601262
+        const puEsperado = 10144.9266661423
 
-    it('Deve calcular o PU em um periodo de um mes, utilizando o fator acumulado', () => {
-        // Massa
-        //let dik = 6.39;
-        //let dp = 20;
-        let porcentagem = 0.06;
-        //let fatordiacumuladoantes = 1.00000000;
-        let vne = 10000;
-
-        // Comportamento
-
-        var i = 0;
-        let fatorDiAcumulado = 1.00000000;
-        let mediaCdi = 1.00000000;
-
-        while (i < mediasCdi.length) {
-            let dp = i + 1;
-
-            const puFinal = calculoPuPos(
-                mediaCdi,
-                porcentagem,
-                dp,
-                fatorDiAcumulado,
-                vne)
-
-            mediaCdi = mediasCdi[i];
-            fatorDiAcumulado = fatorDiAcumulado * calculoFatorDi(new Decimal(mediaCdi));
-            // Resultado esperado
-            expect(puFinal).to.be.equal(puEsperado[i])
-            i++;
-        }
+        expect(puFinal.spread).to.be.equal(spreadEsperado)
+        expect(puFinal.di).to.be.equal(diEsperado)
+        expect(puFinal.diAcumulado).to.be.equal(diAcumuladoEsperado)
+        expect(puFinal.fatorJuros).to.be.equal(fatorJurosEsperado)
+        expect(puFinal.pu).to.be.equal(puEsperado)
     })
 })
 
@@ -79,29 +46,5 @@ describe('Deve realizar o calculo de pu incorretamente de um ativo pré-fixado u
         expect(puFinal.toFixed(12)).to.be.equal('10058.38180897000')
         expect(puFinal.toFixed(12)).to.be.equal(10058.38180897000.toFixed(12))
         expect(puFinal).to.be.equal(10058.38180897000)
-    })
-
-
-    it('Deve calcular o PU em um periodo de um mes', () => {
-        // Massa
-        let porcentagem = 0.158;
-        //let fatordiacumuladoantes = 1.00000000;
-        let vne = 10017.47887;
-
-        // Comportamento
-
-        var i = 0;
-
-        while (i < puEsperadoPre.length) {
-            let dp = i + 1;
-
-            const puFinalPre = calculoPuPre(
-                vne, porcentagem, dp)
-
-            // Resultado esperado
-            expect(puFinalPre).to.be.equal(puEsperadoPre[i])
-            expect(puFinalPre.toFixed(12)).to.be.equal((puEsperadoPre[i]).toFixed(12))
-            i++;
-        }
     })
 })
